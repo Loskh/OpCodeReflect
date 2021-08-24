5e3673eb7bfb0c0a7c4fd52e3c22b089617252a6
PrototypeSeiran <1020612624@qq.com>
2021-08-24 23:11:13 +0800

    Init

---
+++ OpCodeReflect.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+using Machina.FFXIV.Headers;
+using System.IO;
+
+namespace OpCodeReflect
+{
+    public class OpCodeReflect : UserControl, IActPluginV1
+    {
+        private static OpCodeReflectUI PluginUI;
+        private Label _lblStatus; // The status label that appears in ACT's Plugin tab
+        public Dictionary<string, ushort> CurrentOpcodes { get; set; }
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText) {
+            pluginScreenSpace.Text = "OpCodeReflect";
+            PluginUI = new OpCodeReflectUI();
+            PluginUI.InitializeComponent(pluginScreenSpace);
+            if (PluginUI.OpCodePath == "") {
+                var selfPluginData = ActGlobals.oFormActMain.PluginGetSelfData(this);
+                var path = selfPluginData.pluginFile.DirectoryName;
+                var txtPath = path + "\\OpCodes.txt";
+                PluginUI.OpCodePath = txtPath;
+            }
+            Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
+            _lblStatus = pluginStatusText; // Hand the status label's reference to our local var
+            PluginUI.ButtonStart.Click += ButtonStart_Click;
+            PluginUI.ButtonLoad.Click += ButtonLoad_Click;
+            if (PluginUI.AutoStart) {
+                GetOpcodesFromTxt(PluginUI.OpCodePath);
+                SetOpcodes();
+                GetOpcodes();
+            }
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "请选择文件";
+            dialog.Filter = "所有文件(*.txt)|*.txt";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                string file = dialog.FileName;
+                PluginUI.SetPath(file);
+                GetOpcodesFromTxt(file);
+                SetOpcodes();
+                GetOpcodes();
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e) {
+            GetOpcodesFromTxt(PluginUI.OpCodePath);
+            SetOpcodes();
+            GetOpcodes();
+        }
+
+        public void GetOpcodesFromTxt(string txtPath) {
+
+            //https://github.com/ravahn/machina/blob/master/Machina.FFXIV/Headers/Opcodes/OpcodeManager.cs#L55
+            //PluginUI.Log(txtPath);
+            if (!File.Exists(txtPath)) {
+                PluginUI.Log($"{txtPath}不存在！");
+                return;
+            }
+            using (StreamReader sr = new StreamReader(txtPath)) {
+                string[][] data = sr.ReadToEnd()
+                    .Split(new string[] { "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries)).ToArray();
+                var dict = data.ToDictionary(
+                    x => x[0].Trim(),
+                    x => Convert.ToUInt16(x[1].Trim(), 16));
+                CurrentOpcodes = dict;
+            }
+            PluginUI.Log($"已载入{CurrentOpcodes.Count}个OpCode");
+            //foreach (KeyValuePair<string, ushort> kvp in CurrentOpcodes) {
+            //    PluginUI.Log($"{kvp.Key}={kvp.Value:X4}");
+            //}
+        }
+
+        public void Test() {
+            private static IAbilityResource Abilities;
+        }
+
+        public void GetOpcodes() {
+            PluginUI.Log("获取OpCode...");
+            FieldInfo[] opcodes = typeof(Server_MessageType).GetFields();
+            Type type = typeof(Server_MessageType);
+            object obj1 = type.Assembly.CreateInstance(type.FullName);
+            foreach (var opcode in opcodes) {
+                PluginUI.Log($"{opcode.Name}={(ushort)(Server_MessageType)opcode.GetValue(obj1):X4}");
+            }
+        }
+
+        public void SetOpcodes() {
+            PluginUI.Log("设置OpCode...");
+            FieldInfo[] opcodes = typeof(Server_MessageType).GetFields();
+            Type type = typeof(Server_MessageType);
+            object obj1 = type.Assembly.CreateInstance(type.FullName);
+            foreach (var opcode in opcodes) {
+                ushort newOpCode = 0;
+                if (CurrentOpcodes.TryGetValue(opcode.Name, out newOpCode))
+                    opcode.SetValue(obj1, (Server_MessageType)newOpCode);
+                else
+                    PluginUI.Log($"没找到{opcode.Name}");
+            }
+            PluginUI.Log("设置OpCode成功");
+        }
+        public void DeInitPlugin() {
+            PluginUI.SaveSettings();
+        }
+    }
+}
---
+++ OpCodeReflectUI.cs
@@ -0,0 +1,120 @@
+﻿using Advanced_Combat_Tracker;
+using FFXIV_ACT_Plugin.Common;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace OpCodeReflect
+{
+    class OpCodeReflectUI : UserControl
+    {
+
+        private GroupBox _mainGroupBox;
+        public Label LabelOpCodePath;
+
+        public Button ButtonStart { get; private set; }
+        public Button ButtonLoad { get; private set; }
+        private static ListBox lstMessages;
+        private static Button cmdClearMessages;
+        private static Button cmdCopyProblematic;
+        private CheckBox _autoStart;
+
+        public string OpCodePath ="";
+        public bool AutoStart => _autoStart.Checked;
+        private static readonly string SettingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\OpCodeReflect.config.xml");
+
+        public void InitializeComponent(TabPage pluginScreenSpace) {
+            _mainGroupBox = new GroupBox { Location = new Point(12, 12), Text = "日志", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink };
+
+            LabelOpCodePath = new Label { AutoSize = true, Text = $"OpCode路径:{OpCodePath}", Location = new Point(10, 20) };
+            ButtonStart = new Button { Text = "刷新并显示OpCode", Location = new Point(10, LabelOpCodePath.Location.Y+20), Size = new Size(150, 35) };
+            ButtonLoad = new Button { Text = "载入OpCode", Location = new Point(ButtonStart.Location.X+ ButtonStart.Size.Width+20, ButtonStart.Location.Y),Size = new Size(100, 35) };
+            _autoStart = new CheckBox { AutoSize = true, Text = "自动启动", Location = new Point(ButtonLoad.Location.X + ButtonLoad.Size.Width + 20, ButtonLoad.Location.Y) };
+
+            lstMessages = new ListBox { Location = new Point(10, ButtonStart.Location.Y+50), FormattingEnabled = true, ScrollAlwaysVisible = true, HorizontalScrollbar = true, Size = new Size(440, 500), };
+            cmdClearMessages = new Button { Location = new Point(10, lstMessages.Location.Y + lstMessages.Height + 10), Size = new Size(200, 35), Text = "清空日志", UseVisualStyleBackColor = true };
+            cmdCopyProblematic = new Button { Location = new Point(220, lstMessages.Location.Y + lstMessages.Height + 10), Size = new Size(200, 35), Text = "复制到剪贴板", UseVisualStyleBackColor = true };
+
+            _mainGroupBox.Controls.Add(LabelOpCodePath);
+            _mainGroupBox.Controls.Add(ButtonStart);
+            _mainGroupBox.Controls.Add(ButtonLoad);
+            _mainGroupBox.Controls.Add(_autoStart);
+            _mainGroupBox.Controls.Add(lstMessages);
+            _mainGroupBox.Controls.Add(cmdClearMessages);
+            _mainGroupBox.Controls.Add(cmdCopyProblematic);
+
+            pluginScreenSpace.Controls.Add(_mainGroupBox);
+            pluginScreenSpace.AutoSize = true;
+
+            cmdCopyProblematic.Click += cmdCopyProblematic_Click;
+            cmdClearMessages.Click += cmdClearMessages_Click;
+
+            LoadSettings();
+
+            _mainGroupBox.ResumeLayout(false);
+            _mainGroupBox.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        public void cmdCopyProblematic_Click(object sender, EventArgs e) {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (object item in lstMessages.Items) {
+                stringBuilder.AppendLine((item ?? "").ToString());
+            }
+            if (stringBuilder.Length > 0) {
+                Clipboard.SetText(stringBuilder.ToString());
+            }
+        }
+
+        public void cmdClearMessages_Click(object sender, EventArgs e) {
+            lstMessages.Items.Clear();
+        }
+
+        public void Log(string message) {
+            ACTWrapper.RunOnACTUIThread((System.Action)delegate {
+                lstMessages.Items.Add(message);
+            });
+        }
+
+        public void SetPath(string path) {
+            OpCodePath = path;
+            LabelOpCodePath.Text = $"OpCode路径:{OpCodePath}";
+        }
+
+        void LoadSettings() {
+
+            if (File.Exists(SettingsFile)) {
+                XmlDocument xdo = new XmlDocument();
+                try {
+                    xdo.Load(SettingsFile);
+                    XmlNode head = xdo.SelectSingleNode("Config");
+                    SetPath(head?.SelectSingleNode("Path")?.InnerText);
+                    _autoStart.Checked = bool.Parse(head?.SelectSingleNode("AutoStart")?.InnerText ?? "false");
+                }
+                catch (Exception ex) {
+                    Log("配置文件载入异常");
+                    File.Delete(SettingsFile);
+                    Log("已清除错误的配置文件");
+                    Log("设置已被重置");
+                }
+            }
+        }
+
+        public void SaveSettings() {
+            FileStream fs = new FileStream(SettingsFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            XmlTextWriter xWriter = new XmlTextWriter(fs, Encoding.UTF8) { Formatting = Formatting.Indented, Indentation = 1, IndentChar = '\t' };
+            xWriter.WriteStartDocument(true);
+            xWriter.WriteStartElement("Config");    // <Config>
+            xWriter.WriteElementString("Path", OpCodePath);
+            xWriter.WriteElementString("AutoStart", _autoStart.Checked.ToString());
+            xWriter.WriteEndElement();  // </Config>
+            xWriter.WriteEndDocument(); // Tie up loose ends (shouldn't be any)
+            xWriter.Flush();    // Flush the file buffer to disk
+            xWriter.Close();
+        }
+    }
+}
